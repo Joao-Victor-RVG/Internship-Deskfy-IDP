ab1fc1745ac1abc16d67ab0e7a85abecef860f09
João Victor  Rocha Vilela Godoi <joaovictorrochavilelagodoi@MacBook-Air-de-Joao.local>
2023-10-03 19:35:58 -0300

    WIP(Learn and Understand AmgularJS) Common Directives Part-01

--- Learn and Understand AngularJs/03- Data Binding and Directives/4- Common Directives/app.js
+++ Learn and Understand AngularJs/03- Data Binding and Directives/4- Common Directives/app.js
@@ -7,5 +7,16 @@ myApp.controller('mainController', ['$scope', '$filter', function($scope, $filte
     $scope.lowercasehandle = function() {
         return $filter('lowercase')($scope.handle);
     };
+
+    $scope.characters = 5;
+
+    $scope.rules = [
+
+    { rulename: "Must be 5 characters!"},
+    { rulename: "Must not be used elsewhere"},
+    { rulename: "Must be cool"}
+    ];
+
+    console.log($scope.rules);
     
 }]);
